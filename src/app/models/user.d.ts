92708a5dd48db8fc896c0d9d199920978afc6cf5
tanadendere <tanadendere@gmail.com>
2024-04-24 08:19:53 +0200

    feat: :fire: Removing files and commented out code etc... to get PR ready

--- src/app/models/user.d.ts
+++ src/app/models/user.d.ts
@@ -1,7 +1,7 @@
 export interface IUser {
   email: string;
   name: string;
-  surname?: string; // want to use a version of the user where
-  password?: string; // the app won't be able to get the password and username
+  surname: string;
+  password: string;
   userId: string;
 }
