00a2123ab7e298dcdcf33a23bfc139fa708cce7d
tanadendere <tanadendere@gmail.com>
2024-05-02 05:50:03 +0200

    feat: :sparkles: Happy path for user state management login and register

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,12 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { loginUser } from '../../store/actions';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../models/state';
+import { Observable } from 'rxjs';
+import { IUser } from '../../models/user';
+import { selectUser } from '../../store/selectors';
 
 @Component({
   selector: 'app-login',
@@ -13,6 +19,8 @@ export class LoginComponent {
   fb = inject(FormBuilder);
   authService = inject(AuthService);
   router = inject(Router);
+  store: Store<AppState> = inject(Store);
+  user$: Observable<IUser | undefined> = this.store.select(selectUser);
 
   form = this.fb.nonNullable.group({
     email: ['', Validators.required],
@@ -22,13 +30,23 @@ export class LoginComponent {
 
   onSubmit(): void {
     const rawForm = this.form.getRawValue();
-    this.authService.login(rawForm.email, rawForm.password).subscribe({
-      next: () => {
-        this.router.navigateByUrl('/'); //redirect to homePage
-      },
-      error: (err) => {
-        this.errorMessage = err.code;
-      },
+    this.store.dispatch(
+      loginUser({ email: rawForm.email, password: rawForm.password })
+    );
+    // this.authService.login(rawForm.email, rawForm.password).subscribe({
+    //   next: () => {
+    //     this.router.navigateByUrl('/'); //redirect to homePage
+    //   },
+    //   error: (err) => {
+    //     this.errorMessage = err.code;
+    //   },
+    // });
+    this.user$.subscribe((user) => {
+      if (user != undefined) {
+        console.log(user);
+        this.router.navigateByUrl('trips');
+      }
+      console.log('in register');
     });
   }
 }
