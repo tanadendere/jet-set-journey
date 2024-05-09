bb55d8bbfe26d40031ed1298fd13ce64927b2a35
tanadendere <tanadendere@gmail.com>
2024-05-09 09:44:08 +0200

    feat: :art: Changes from PR review comments

--- src/app/userManagement/models/currency.d.ts
+++ src/app/userManagement/models/currency.d.ts
@@ -1,11 +1,11 @@
 export interface ICurrency {
   symbol: string;
   name: string;
-  symbol_native: string;
-  decimal_digits: number;
+  symbolNative: string;
+  decimalDigits: number;
   rounding: number;
   code: string;
-  name_plural: string;
+  namePlural: string;
   type: string;
   countries: string[];
 }
