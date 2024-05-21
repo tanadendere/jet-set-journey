ce9463db5f483be4a9933f8be33fd42e78e47b59
tanadendere <tanadendere@gmail.com>
2024-05-21 16:22:20 +0200

    feat: :sparkles: Click on logo to go to home and use curser pointer for clickable elements

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,13 +4,14 @@ import { CurrencySelectionComponent } from '../../userManagement/components/curr
 import { UserState } from '../../models/state';
 import { Store } from '@ngrx/store';
 import { logoutUser } from '../../userManagement/store/actions';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
-  imports: [CurrencySelectionComponent],
+  imports: [CurrencySelectionComponent, RouterLink],
 })
 export class HeaderComponent {
   userStore: Store<UserState> = inject(Store);
