321d84a1ca37cccb4b65634a0530de6f6f017a03
tanadendere <tanadendere@gmail.com>
2024-05-24 15:06:13 +0200

    feat: :lipstick: The UI for the Itinerary cards

--- src/app/tripManagement/components/trip-details/itinerary-item/itinerary-item.component.ts
+++ src/app/tripManagement/components/trip-details/itinerary-item/itinerary-item.component.ts
@@ -30,6 +30,17 @@ export class ItineraryItemComponent {
     });
   }
 
+  getTime(dateString: string) {
+    const date = new Date(dateString);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const formattedHours = hours.toString().padStart(2, '0');
+    const formattedMinutes = minutes.toString().padStart(2, '0');
+
+    return formattedHours + ':' + formattedMinutes;
+  }
+
   deleteItem() {
     if (this.trip && this.item.itemId) {
       this.store.dispatch(
